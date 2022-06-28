e76b85c0297f67be8445edab5570c205b513132e
katsuya_U <katsuya_u@icloud.com>
2022-06-28 23:54:41 +0900

    setup react-router and vite-plugin-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import React from "react"
+import React, { Suspense } from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter, useRoutes } from "react-router-dom"
 
-import App from "./App"
-import "./index.css"
+import _routes from "~react-pages"
+
+console.log(_routes)
+
+const routes = [..._routes, {}]
+
+const App = () => <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
---
+++ src/samples/202206-hello-vite-plugin-pages/index.tsx
@@ -0,0 +1,10 @@
+// https://github.com/hannoeru/vite-plugin-pages
+import React from "react"
+
+export default function Page() {
+  return (
+    <div>
+      <h2>202206-hello-vite-plugin-pages</h2>
+    </div>
+  )
+}
