db9aa331a3fd98cac808c3e8b462e9c6c212c206
katsuyaU <katsuyaU@users.noreply.github.com>
2022-06-28 18:26:31 +0000

    deploy: c9cda770e6092b30a4bc7bfab99f999e901625e2

--- assets/index.77d5d807.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index.a3a2ac90.js";function r(){return e("div",{children:e("h2",{children:"hello-vite-plugin-pages"})})}export{r as default};
---
+++ assets/index.d3cf66de.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.4afee376.js";function r(){return e("div",{children:e("h2",{children:"hello-vite-plugin-pages"})})}export{r as default};
