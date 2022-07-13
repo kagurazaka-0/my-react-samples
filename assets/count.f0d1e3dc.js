53bd51e2ba9df4976f597d20e665e88e0f0a0395
kagurazaka-0 <kagurazaka-0@users.noreply.github.com>
2022-07-13 15:37:30 +0000

    deploy: e0d2f119ec45ab5f682448bf48f54564f53f6efa

---
+++ assets/count.f0d1e3dc.js
@@ -0,0 +1,1 @@
+import{a as e,b as o}from"./index.8e8d9d77.js";const n=e({key:"countState",default:0}),c=o({key:"countPlus1State",get:({get:t})=>t(n)+1});export{c as countPlus1State,n as countState};
