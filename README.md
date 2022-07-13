e0d2f119ec45ab5f682448bf48f54564f53f6efa
katsuya_U <katsuya_u@icloud.com>
2022-07-14 00:36:55 +0900

    add page "recoil-sample"

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 <!-- prettier-ignore -->
 |名称|概要|
 |-|-|
-|[type-safe-html-select](https://kagurazaka-0.github.io/my-react-samples/202207-type-safe-html-select/)||
+|[recoil-sample](https://kagurazaka-0.github.io/my-react-samples/202207-recoil-sample/)|Recoil の API を検証するサンプル|
 |[hello-vite-plugin-pages](https://kagurazaka-0.github.io/my-react-samples/202206-hello-vite-plugin-pages/)|vite-plugin-pages が動いているかを確かめるためのサンプル|
 
 ## development
--- src/samples/202207-recoil-sample/update-count.tsx
+++ src/samples/202207-recoil-sample/update-count.tsx
@@ -1,15 +1,17 @@
 import React from "react"
-import { useRecoilState } from "recoil"
+import { useRecoilState, useRecoilValue } from "recoil"
 
-import { countState } from "./state/count"
+import { countPlus1State, countState } from "./state/count"
 
 export default function Page() {
   const [count, setCount] = useRecoilState(countState)
+  const countPlus1 = useRecoilValue(countPlus1State)
 
   return (
     <div>
       <h2>recoil-sample/update-count</h2>
       <p>countState = {count}</p>
+      <p>countPlus1State = {countPlus1}</p>
       <div style={{ display: "flex", gap: "8px" }}>
         <button onClick={() => setCount((it) => it + 1)}>+1</button>
         <button onClick={() => setCount((it) => it - 1)}>-1</button>
