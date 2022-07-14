497086ed09f24764d0499674400265cbb627bd2b
katsuya_U <katsuya_u@icloud.com>
2022-07-14 21:33:54 +0900

    add vite-tsconfig-paths and config alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
 import pages from "vite-plugin-pages"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
   base: loadEnv(mode, "./").VITE_BASEURL,
   plugins: [
+    tsconfigPaths(),
     react(),
     pages({
       dirs: "src/samples",
