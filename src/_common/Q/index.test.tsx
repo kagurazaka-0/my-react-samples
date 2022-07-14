e665269eac812b351656de164eda6de76bdeb332
katsuya_U <katsuya_u@icloud.com>
2022-07-14 22:20:01 +0900

    setup vitest

---
+++ src/_common/Q/index.test.tsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react"
+import * as assert from "assert/strict"
+import React from "react"
+
+import { Q } from "./index"
+
+// TODO: https://zenn.dev/longbridge/articles/9d9ec773cb3814 を参考にsetupする
+// TODO: テスト書く
+describe("Q --- util components for class props", () => {
+  it("", () => {
+    const { container } = render(<Q.div class=""></Q.div>)
+  })
+})
