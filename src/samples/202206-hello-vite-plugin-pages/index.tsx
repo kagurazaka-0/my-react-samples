fae0fbfca18d44abad0a23862a6522841e3bc56b
katsuya_U <katsuya_u@icloud.com>
2022-07-20 04:53:39 +0900

    feat long page

--- src/samples/202206-hello-vite-plugin-pages/index.tsx
+++ src/samples/202206-hello-vite-plugin-pages/index.tsx
@@ -1,10 +1,17 @@
 // https://github.com/hannoeru/vite-plugin-pages
 import React from "react"
 
+const LIST = Array.from({ length: 100 }, (_, i) => i)
+
 export default function Page() {
   return (
     <div>
       <h2>hello-vite-plugin-pages</h2>
+      <div className="mt-4 space-y-2">
+        {LIST.map((i) => {
+          return <div key={i}>{i}</div>
+        })}
+      </div>
     </div>
   )
 }
