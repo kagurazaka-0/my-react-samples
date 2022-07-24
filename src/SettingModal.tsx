243c3e8dbd01bafc3bc60013f15586167d0e2d6d
katsuya_U <katsuya_u@icloud.com>
2022-07-24 21:07:34 +0900

    feat: 設定を変更したら自動保存するように実装

--- src/SettingModal.tsx
+++ src/SettingModal.tsx
@@ -1,54 +1,91 @@
+import { useDebugValue, useEffect, useRef } from "react"
 import { useForm } from "react-hook-form"
 
-import IconSave from "~icons/ant-design/save-filled"
+import IconClose from "~icons/ant-design/close-outlined"
+import IconInfo from "~icons/ant-design/info-circle"
 import IconSetting from "~icons/ant-design/setting"
 
 import { Q } from "~/_common/Q"
 import { COLORS, useColorState } from "~/_common/daisyui/color-state"
 import { useBoolean } from "~/_common/hooks/useBoolean"
 
+function useWatch<T>(watchTarget: T, fn: (watchValue: T) => void) {
+  const isMountedRef = useRef(false)
+
+  useEffect(() => {
+    if (!isMountedRef.current) {
+      isMountedRef.current = true
+      return
+    }
+    fn(watchTarget)
+  }, [watchTarget])
+
+  if (IS_DEV) {
+    useDebugValue(watchTarget)
+  }
+}
+
 export function SettingModal() {
-  const [isOpenModal, { on, off }] = useBoolean()
+  const [isOpenModal, { on: openModal, off: closeModal }] = useBoolean()
+  const [isSaved, { on: setIsSavedTrue, off: setIsSavedFalse }] = useBoolean()
 
   const [currentColor, setColor] = useColorState()
 
   type FormType = {
     color: string
   }
-  const { register, handleSubmit, reset, formState } = useForm<FormType>()
+  const { register, reset, watch } = useForm<FormType>({ defaultValues: { color: currentColor } })
 
-  const onClickOpen = () => {
-    on()
+  const onClickOpen = async () => {
     reset({ color: currentColor })
+    openModal()
   }
 
-  const onSubmit = handleSubmit(({ color }) => {
+  const onClickClose = () => {
+    closeModal()
+    setIsSavedFalse()
+  }
+
+  useWatch(watch("color"), (color) => {
+    if (!isOpenModal) {
+      return
+    }
     setColor(color)
-    off()
-    // TODO: 保存しましたのトースト
+    setIsSavedTrue()
   })
 
   return (
     <>
-      <Q.span class="ds-btn ds-btn-circle fixed bottom-4 right-4 z-[10000] " onClick={onClickOpen}>
+      <Q.span class="ds-btn ds-btn-circle fixed bottom-4 right-4 z-50" onClick={onClickOpen}>
         <IconSetting className="h-6 w-6" />
       </Q.span>
 
       <Q.div class={["ds-modal", isOpenModal && "ds-modal-open"]}>
         <Q.div class="ds-modal-box relative bg-base-300">
           <Q.div class="flex items-center text-lg font-bold">
-            <IconSetting className="mr-2 h-[1.4em] w-[1.4em]" />
-            <Q.span>設定</Q.span>
+            <IconSetting className="h-[1.4em] w-[1.4em]" />
+            <Q.span class="ml-2">設定</Q.span>
+            <Q.div
+              class={[
+                "ds-alert ds-alert-success ds-alert-sm shadow-lg ml-4 w-fit transition duration-300",
+                !isSaved && "opacity-0 -translate-x-5",
+              ]}
+            >
+              <Q.div class="text-sm">
+                <IconInfo className="stroke-current flex-shrink-0 h-[1.4em] w-[1.4em]" />
+                <Q.span>保存しました</Q.span>
+                <Q.span class="ds-btn ds-btn-ghost ds-btn-sm ds-btn-square" onClick={setIsSavedFalse}>
+                  <IconClose />
+                </Q.span>
+              </Q.div>
+            </Q.div>
           </Q.div>
-          <Q.span class="ds-btn ds-btn-circle ds-btn-sm absolute right-4 top-4" onClick={off}>
-            ✕
+          <Q.span class="ds-btn ds-btn-circle ds-btn-sm absolute right-4 top-4" onClick={onClickClose}>
+            <IconClose />
           </Q.span>
-          <Q.form class="ds-form-control pt-4" onSubmit={onSubmit}>
+          <Q.form class="ds-form-control py-4">
             <Q.label class="ds-label">
-              <Q.span class="ds-label-text">
-                daisyUI テーマ
-                {formState.dirtyFields.color && " *"}
-              </Q.span>
+              <Q.span class="ds-label-text">daisyUI テーマ</Q.span>
             </Q.label>
             <Q.select class="ds-select ds-select-bordered" {...register("color")}>
               {COLORS.map((color) => (
@@ -57,12 +94,6 @@ export function SettingModal() {
                 </Q.option>
               ))}
             </Q.select>
-            <Q.div class="mt-6 flex justify-end">
-              <Q.button class="ds-btn" type="submit">
-                <IconSave className="mr-2 h-[1.4em] w-[1.4em]" />
-                保存する
-              </Q.button>
-            </Q.div>
           </Q.form>
         </Q.div>
       </Q.div>
