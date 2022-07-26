46660e918e4a96935a44a94b3956376a271ee8ed
katsuya_U <katsuya_u@icloud.com>
2022-07-26 20:28:40 +0900

    chore pretteir plugin hack

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,21 @@
+const pluginSortImports = require("@trivago/prettier-plugin-sort-imports")
+const pluginTailwindcss = require("prettier-plugin-tailwindcss")
+
+/** @type {import("prettier").Parser}  */
+const hackParser = {
+  ...pluginSortImports.parsers.typescript,
+  parse: pluginTailwindcss.parsers.typescript.parse,
+}
+
+/** @type {import("prettier").Plugin}  */
+const hackedPlugin = {
+  parsers: {
+    typescript: hackParser,
+  },
+}
+
 module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss"), require("@trivago/prettier-plugin-sort-imports")],
+  plugins: [hackedPlugin],
   tabWidth: 2,
   semi: false,
   printWidth: 120,
