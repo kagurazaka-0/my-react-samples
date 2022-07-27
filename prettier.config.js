9e7af5f00a508220f8d90a90979407329f661937
katsuya_U <katsuya_u@icloud.com>
2022-07-28 06:54:21 +0900

    fix prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,24 @@
+///@ts-check
+
 const pluginSortImports = require("@trivago/prettier-plugin-sort-imports")
+const pluginOrganizeImports = require("prettier-plugin-organize-imports")
 const pluginTailwindcss = require("prettier-plugin-tailwindcss")
+
+const { parsers: typescriptParsers } = require("prettier/parser-typescript")
 
 /** @type {import("prettier").Parser}  */
 const myParser = {
-  ...pluginSortImports.parsers.typescript,
+  ...typescriptParsers.typescript,
+  preprocess(text, options) {
+    let it = text
+    try {
+      it = pluginOrganizeImports.parsers.typescript.preprocess(it, options)
+      it = pluginSortImports.parsers.typescript.preprocess(it, options)
+    } catch (error) {
+      console.warn(`⚠️ plugin error`, error)
+    }
+    return it
+  },
   parse: pluginTailwindcss.parsers.typescript.parse,
 }
 
