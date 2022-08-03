db6105c45acfa79accb58eb0bf28e59d7bf71041
katsuya_U <katsuya_u@icloud.com>
2022-08-03 13:46:43 +0900

    feat welcome page

--- src/samples/index.tsx
+++ src/samples/index.tsx
@@ -1,3 +1,17 @@
+import { Q } from "~/_common/Q"
+
 export default function Page() {
-  return <></>
+  return (
+    <Q.div class="rounded-box h-[calc(100vh-120px)] w-full bg-gradient-to-br from-primary to-secondary text-primary-content center">
+      <Q.div class="max-w-md">
+        <Q.div class="text-4xl font-bold">Welcome to</Q.div>
+        <Q.div class="mt-2 space-x-1 text-3xl font-semibold">
+          <Q.span class="text-base-content">My</Q.span>
+          <Q.span class="text-secondary-content">React</Q.span>
+          <Q.span class="text-base-content">Samples</Q.span>
+        </Q.div>
+      </Q.div>
+    </Q.div>
+  )
+  // <Q.div class=""></Q.div>
 }
