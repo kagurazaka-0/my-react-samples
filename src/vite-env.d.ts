b1a4eec09153bb765e13bd4ccf01c51e97ffe05b
katsuya_U <katsuya_u@icloud.com>
2022-09-15 13:19:22 +0900

    fix package json and more

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client-react" />
 /// <reference types="unplugin-icons/types/react" />
+/// <reference types="@hookform/resolvers/zod" />
 
 // 既存のenv定義に追記している
 interface ImportMetaEnv {
