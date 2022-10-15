ee47760b5d308291aab25f160871af273f2c0b84
katsuya_U <katsuya_u@icloud.com>
2022-10-15 15:04:29 +0900

    fix new sample

--- scripts/new-sample.ts
+++ scripts/new-sample.ts
@@ -1,16 +1,17 @@
 import format from "https://deno.land/x/date_fns@v2.22.1/format/index.js"
 import { exec } from "https://raw.githubusercontent.com/kagurazaka-0/deno-scripts/main/utils/exec.ts"
 
-const name = Deno.args[0] ?? prompt("sample name?")
+const name = Deno.args[0] || prompt("sample name?")
 if (!name) {
   console.warn("⚠️  name is empty.")
   Deno.exit(1)
 }
 
 const description = prompt("description? (optional)") ?? ""
+const isPrivate = (prompt("private? (y/N)") ?? "n").toLowerCase() === "y"
 
 const dirName = `${format(new Date(), "yyyyMMdd", {})}-${name}`
-const dirPath = `src/samples/${dirName}`
+const dirPath = `src/samples/${isPrivate ? "_" : ""}${dirName}`
 
 Deno.mkdir(dirPath)
 
@@ -24,5 +25,5 @@ ${description}
 
 `)
 
-await exec(`npx prettier --write ${dirPath}`)
+await exec(`npx prettier --write ${dirPath}/*`)
 await exec(`npm run generate-readme`)
