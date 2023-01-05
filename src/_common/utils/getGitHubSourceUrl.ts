a027ba5db8576c2bff7a2c07285b92a1de1331fa
katsuya_U <katsuya_u@icloud.com>
2023-01-05 16:04:50 +0900

    fix

--- src/_common/utils/getGitHubSourceUrl.ts
+++ src/_common/utils/getGitHubSourceUrl.ts
@@ -1,8 +1,6 @@
-import { join } from "path-browserify"
-
 const REPO_URL = "https://github.com/kagurazaka-0/my-react-samples"
 
 export function getGitHubSourceUrl(path: string) {
-  return join(REPO_URL, "blob/main", path)
+  return [REPO_URL, "blob/main", path].join("/")
   // return `${REPO_URL}/blob/main${path}`
 }
